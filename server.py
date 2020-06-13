41d0c63f84b6c653cf56a6f4f32c7ada3aa994fe
Alexander Wong <alexjwong@berkeley.edu>
2020-06-13 14:04:43 -0700

    server hello world

---
+++ server.py
@@ -0,0 +1,11 @@
+import json
+from flask import Flask, render_template, request # pylint: disable=import-error
+
+HOST_URL = "127.0.0.1" # "127.0.0.1"
+PORT = 5500
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def hello():
+    return 'hello world'
