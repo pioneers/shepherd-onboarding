1b686c8fb5335ebbb91d0cf46c3f9f63190fc8fb
Alexander Wong <alexjwong@berkeley.edu>
2020-06-13 14:21:46 -0700

    added utils, testing push from intellij

---
+++ Utils.py
@@ -0,0 +1,8 @@
+class STATE():
+    SETUP = "setup"
+    NOMINATION = "nomination"
+    VOTE = "vote"
+    PRESIDENT_DISCARD = "president_discard"
+    CHANCELLOR_DISCARD = "chancellor_discard"
+    EXECUTIVE_ACTION = "executive_action"
+    GAME_OVER = "game_over"
