94aad3b388bf6e66be5cdfb1cdb40d67fd2cf710
Matthew Harrigan <betterthanbossfiredog5@gmail.com>
2020-06-17 11:50:06 -0700

    added setup phase state machine logic

--- Shepherd.py
+++ Shepherd.py
@@ -1,6 +1,12 @@
 from Utils import *
+from LCM import lcm_send
+import random
 
 def LCM_receive(header, dic={}):
+    """
+    this dispatches LCM requests from the server in a way that mimics the
+    asynchronous dispatching that normally happens in shepherd
+    """
     global GAME_STATE
     print("GAME STATE OUTSIDE: ", GAME_STATE)
     print(header, dic)
@@ -48,27 +54,98 @@ def LCM_receive(header, dic={}):
 # game functions
 #===================================
 
-def player_joined(args):
+def to_setup(args):
+    """
+    A function that moves the game into setup phase.
+    """
+    reset()
+    CARD_DECK = new_deck()
+    GAME_STATE = STATE.SETUP
+    # this needs to be the last thing that happens, since due to how this
+    # pseudo-LCM actually works, this will do a signifigant amount of work on
+    # the server's side, and may spawn more LCM messages.
+    lcm_send(LCM_TARGETS.SERVER, SERVER_HEADERS.FORCE_RECONNECT, {})
+
+
+def player_joined_new_game(args):
+    """
+    A function that creates a new player instance for a player who has joined,
+    or will allow a player to reconnect if they have joined before.
+    """
     global PLAYERS
     id = args["id"]
     name = args["name"]
-    #send data to player with that id
-    if not id in PLAYERS:
+    lcm_data = {"usernames" : player_names(PLAYERS)}
+    if not id in player_ids(PLAYERS):
+        # is this someone reconnecting or joining for the first time?
         PLAYERS += [Player(id, name)]
-        #send data about that player joining to everyone
+        lcm_data["recipients" = player_ids(PLAYERS)]
+    else:
+        lcm_data["recipients" = [id]]
+    lcm_send(LCM_TARGETS.SERVER, SERVER_HEADERS.PLAYERS, lcm_data)
 
+def player_joined_ongoing_game(args):
+    """
+    A function that allows players to reconnect to the game.
+    """
+    global PLAYERS
+    id = args["id"]
+    name = args["name"]
+    if id in player_ids(PLAYERS):
+        # is this someone reconnecting or joining for the first time?
+        lcm_data = {"usernames" : player_names(PLAYERS), "recipients" : [id]}
+        lcm_send(LCM_TARGETS.SERVER, SERVER_HEADERS.PLAYERS, lcm_data)
+        # TODO: there is more stuff to send to the player probably
+
+#===================================
+# helper functions
+#===================================
+
+def player_names(players):
+    return [player.name for player in players]
+
+def player_ids(players):
+    return [player.id for player in players]
+
+def reset():
+    """
+    Retuns the game state to what it was at the begining of executions
+    """
+    PLAYERS = []
+    CARD_DECK = []
+    PLAYER_INDEX = 0
+    PREVIOUS_PLAYER_INDEX = 0
+    ELECTION_TRACKER = 0
+
+def shuffle_deck(deck):
+    random.shuffle(deck)
+
+def new_deck():
+    new_deck = [CARDS.LIBERAL for _ in range(6)]
+    new_deck += [CARDS.FACIST for _ in range(11)]
+    shuffle_deck(new_deck)
+    return new_deck
+
+def next_player_index():
+    global PLAYER_INDEX, PLAYERS
+    return (PLAYER_INDEX + 1) % len(PLAYERS)
 
 #===================================
 # game variables
 #===================================
 
-GAME_STATE = STATE.SETUP
+GAME_STATE = STATE.END
 
 PLAYERS = []
+CARD_DECK = []
+PLAYER_INDEX = 0
+PREVIOUS_PLAYER_INDEX = 0
+ELECTION_TRACKER = 0
 
-SETUP_FUNCTIONS = {}
-END_FUNCTIONS = {}
-CHANCELLOR_FUNCTIONS = {}
-VOTE_FUNCTIONS = {}
-POLICY_FUNCTIONS = {}
-ACTION_FUNCTIONS = {}
+SETUP_FUNCTIONS = {SHEPHERD_HEADERS.PLAYER_JOINED : player_joined_new_game}
+CHANCELLOR_FUNCTIONS = {SHEPHERD_HEADERS.PLAYER_JOINED : player_joined_ongoing_game}
+VOTE_FUNCTIONS = {SHEPHERD_HEADERS.PLAYER_JOINED : player_joined_ongoing_game}
+POLICY_FUNCTIONS = {SHEPHERD_HEADERS.PLAYER_JOINED : player_joined_ongoing_game}
+ACTION_FUNCTIONS = {SHEPHERD_HEADERS.PLAYER_JOINED : player_joined_ongoing_game}
+END_FUNCTIONS = {SHEPHERD_HEADERS.PLAYER_JOINED : player_joined_ongoing_game,
+                 SHEPHERD_HEADERS.NEXT_STAGE : to_setup}
--- Utils.py
+++ Utils.py
@@ -1,4 +1,40 @@
 # pylint: disable=invalid-name
+class SHEPHERD_HEADERS():
+    PLAYER_JOINED = "player_joined"
+    """
+    Header sent to shepherd whenever a player joins or reconnects to the server
+    contains:
+        name - a string with the name of the user
+        id   - a string with the id of the user, calculated as a hash between
+               the username and a password
+    """
+
+    NEXT_STAGE = "next_stage"
+    """
+    Header used to tell shepherd that the current phase is over and to advance.
+    This is only useful for moving from END->SETUP and SETUP->CHANCELLOR.
+    """
+
+# pylint: disable=invalid-name
+class SERVER_HEADERS():
+    PLAYERS = "players"
+    """
+    Header sent to the server either because a player has reconnected and needs
+    to be brought up to speed, or because a new player has connected.
+    contains:
+        usernames  - an ordered list of usernames
+        recipients - a list of players by id who should recieve this update. A
+                     server may choose not to implement this, at the cost of
+                     some extra network usage.
+    """
+
+    FORCE_RECONNECT = "force_reconnect"
+    """
+    Header sent to server to force all clients to send a PLAYER_JOINED header
+    back to shepherd
+    """
+
+# pylint: disable=invalid-name
 class STATE():
     SETUP = "setup"
     END = "end"
@@ -10,6 +46,7 @@ class STATE():
 # pylint: disable=invalid-name
 class LCM_TARGETS():
     SHEPHERD = "lcm_target_shepherd"
+    SERVER = "lcm_target_server"
 
 class VOTES():
     JA = "ja"
@@ -20,3 +57,7 @@ class ROLES():
     FACIST = "facist"
     LIBERAL = "liberal"
     HITLER = "hitler"
+
+class CARDS():
+    FACIST = "facist_card"
+    LIBERAL = "liberal_card"
