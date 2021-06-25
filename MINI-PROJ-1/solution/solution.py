dd2cebf012527960bdb852f6f0d899cf4919336f
Jonathan <jonathanpatkins@DESKTOP-UP1ADP5.localdomain>
2021-06-25 11:39:18 -0700

    mini-proj-1

---
+++ MINI-PROJ-1/solution/solution.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask.templating import render_template
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/solution/')
+def hello_world():
+    return render_template("solution.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
